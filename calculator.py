963d66382cf09b48ea73e61ef42b8af5569cf605
Kawasaaki <134572447+Kawasaaki@users.noreply.github.com>
2023-05-28 22:27:02 +0545

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,20 @@
+a = int(input("Enter any number"))
+b = int(input("Enter any number"))
+
+sum = a + b
+print("The sum of a and b is :," ,sum)
+
+mul = a * b
+print("The multiplication of a and b is :" ,mul)
+
+sub = a - b
+print("The subtraction is :",sub)
+
+exp = a ** b
+print("The exponential is ",exp)
+
+div = a/b
+print("The division is" ,div)
+
+mod = a % b
+print("The modulus is" ,mod)
